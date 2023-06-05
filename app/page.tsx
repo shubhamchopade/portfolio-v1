7ef44efe6c53ac30cf35496136d672f0d8929674
Shubham Chopade <31898834+shubhamchopade@users.noreply.github.com>
2023-06-04 22:40:40 -0400

    chore: og image

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,16 @@ import { info } from "@/constants/site";
 export default function Home() {
   return (
     <div className="">
-      <meta property="og:image" content={info.metadata.og.image} />
+      <meta
+        property="og:image"
+        itemProp="image"
+        content={info.metadata.og.image}
+      />
       <meta property="og:title" content={info.metadata.og.title} />
       <meta property="og:description" content={info.bio} />
       <meta property="og:url" content={info.metadata.og.url} />
+      <meta property="og:type" content="website" />
+      <meta property="og:updated_time" content={Date.now().toString()} />
 
       <meta property="twitter:image" content={info.metadata.twitter.image} />
       <meta property="twitter:card" content={info.metadata.twitter.card} />
