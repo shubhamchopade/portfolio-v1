857ff55f706449bfe4d163cc10d563f0999c3cca
Shubham Chopade <31898834+shubhamchopade@users.noreply.github.com>
2023-06-07 18:05:47 -0400

    sort posts

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -3,9 +3,12 @@ import { info } from "@/constants/site";
 import Link from "next/link";
 
 export default function Home() {
+  const sortedPosts = allPosts.sort((a, b) => {
+    return new Date(b.date).getTime() - new Date(a.date).getTime();
+  });
   return (
     <div className="prose dark:prose-invert">
-      {allPosts.map((post) => (
+      {sortedPosts.map((post) => (
         <article className="border px-8 mb-4" key={post._id}>
           <Link href={post.slug}>
             <h2>{post.title}</h2>
