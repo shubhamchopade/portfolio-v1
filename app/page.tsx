52799518f424f3075ca7f2d6af4ae5a9e838104f
Shubham Chopade <31898834+shubhamchopade@users.noreply.github.com>
2024-05-25 20:02:06 -0400

    remove unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
-import { allPosts } from "@/.contentlayer/generated";
 import Education from "@/components/education";
 import Experience from "@/components/experience";
 import Landing from "@/components/landing";
-import Pomodoro from "@/components/pomodoro";
 import Projects from "@/components/projects";
 import Timeline from "@/components/timeline";
-import Link from "next/link";
-import { info } from "@/constants/site";
 
 export default function Home() {
   return (
